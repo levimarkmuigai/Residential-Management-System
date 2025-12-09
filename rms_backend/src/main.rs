d3bf3bdfb122d0990115e4b348adf658cd7b6e87
levimarkmuigai <levimarkmuigai@gmail.com>
2025-12-09 11:38:01 +0300

    feat(Network Layering)

--- rms_backend/src/main.rs
+++ rms_backend/src/main.rs
@@ -1,3 +1,27 @@
+use std::io::Write;
+use std::net::{TcpListener, TcpStream};
+
 fn main() {
-    println!("Hello, world!");
+    // Binding
+    let listener: TcpListener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    println!("Server running on 127.0.0.1:7878");
+
+    // Iteration over an infinite stream of connection attempts
+    for stream_result in listener.incoming() {
+        match stream_result {
+            Ok(stream) => {
+                handle_connection(stream);
+            }
+            Err(e) => {
+                eprint!("Failed to establish connection: {}", e);
+            }
+        }
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    println!("New Connection Established!");
+
+    let _ = stream.write_all(b"Hello from Rust!");
 }
---
+++ rms_backend/target/debug/deps/rms_backend-158408bd1f3e6af2.d
@@ -0,0 +1,5 @@
+/home/levi/Projects/Residential-Management-System/rms_backend/target/debug/deps/rms_backend-158408bd1f3e6af2.d: src/main.rs
+
+/home/levi/Projects/Residential-Management-System/rms_backend/target/debug/deps/librms_backend-158408bd1f3e6af2.rmeta: src/main.rs
+
+src/main.rs:
