71ce4a639aa6102919c0db5b451541648400623d
levimarkmuigai <levimarkmuigai@gmail.com>
2026-01-26 20:19:25 +0300

    feat(frontend)

--- rms_backend/target/debug/deps/rms_backend-a072611a01c16fd9.d
+++ rms_backend/target/debug/deps/rms_backend-a072611a01c16fd9.d
@@ -1,10 +1,11 @@
-/home/levi/Prototypes/Residential-Management-System/rms_backend/target/debug/deps/rms_backend-a072611a01c16fd9.d: src/main.rs src/http.rs src/db.rs src/data.rs src/user.rs src/login.rs
+/home/levi/Prototypes/Residential-Management-System/rms_backend/target/debug/deps/rms_backend-a072611a01c16fd9.d: src/main.rs src/server.rs src/db.rs src/data.rs src/user.rs src/login.rs src/signup.rs
 
-/home/levi/Prototypes/Residential-Management-System/rms_backend/target/debug/deps/rms_backend-a072611a01c16fd9: src/main.rs src/http.rs src/db.rs src/data.rs src/user.rs src/login.rs
+/home/levi/Prototypes/Residential-Management-System/rms_backend/target/debug/deps/rms_backend-a072611a01c16fd9: src/main.rs src/server.rs src/db.rs src/data.rs src/user.rs src/login.rs src/signup.rs
 
 src/main.rs:
-src/http.rs:
+src/server.rs:
 src/db.rs:
 src/data.rs:
 src/user.rs:
 src/login.rs:
+src/signup.rs:
